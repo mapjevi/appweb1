6073314420abc59fca71c7b0223dadae4a16957e
Giondo <giondog@gmail.com>
2018-09-27 13:13:57 -0300

    mod index.php

--- index.php
+++ index.php
@@ -9,8 +9,21 @@
 
 <img src="ticforum.png">
 </div>
-</body>
+
 <?php
-phpinfo(INFO_MODULES);
+    foreach($phpinfo as $name => $section) {
+        echo "<h3>$name</h3>\n<table>\n";
+        foreach($section as $key => $val) {
+            if(is_array($val))
+                echo "<tr><td>$key</td><td>$val[0]</td><td>$val[1]</td></tr>\n";
+            elseif(is_string($key))
+                echo "<tr><td>$key</td><td>$val</td></tr>\n";
+            else
+                echo "<tr><td>$val</td></tr>\n";
+        }
+        echo "</table>\n";
+    }
 ?>
+
+</body>
 </html>

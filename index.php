08d420027d9cc131ff916194d93f616ca9d4364d
Giondo <giondog@gmail.com>
2018-09-27 12:51:03 -0300

    mod index.php

--- index.php
+++ index.php
@@ -5,14 +5,6 @@
 <body style="background-color:#F9F8F8;">
 <h1 align=center > Deploy 4 </h1>
 
-<style>
-img {
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-}
-</style>
-
 <img src="ticforum.png" class="center">
 <?php
 phpinfo();

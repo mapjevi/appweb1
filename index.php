6412560cd0a648e4079198f0c8081a8c0658648d
Giondo <giondog@gmail.com>
2018-09-27 12:49:35 -0300

    mod index.php

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
 <html>
-<body style="background-color:#F9F8F8;">
-<img src="ticforum.png" class="center">
 <head>
 </head>
 <body style="background-color:#F9F8F8;">
 <h1 align=center > Deploy 4 </h1>
 
-<meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
 img {
     display: block;
@@ -16,6 +13,7 @@ img {
 }
 </style>
 
+<img src="ticforum.png" class="center">
 <?php
 phpinfo();
 ?>
